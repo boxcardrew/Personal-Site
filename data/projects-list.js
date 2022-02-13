2f3cfd607273b7576dc4c833001591b2169197ce
Drew Bray <bray.drew@gmail.com>
2022-02-13 08:03:22 -0700

    Update projects-list.js

--- data/projects-list.js
+++ data/projects-list.js
@@ -2,28 +2,28 @@ const projects = [
   {
     title: "All About Echo",
     tags: ["UI/UX", "Logo", "Development"],
-    excerpt: "This is a simple wordpress site. I mostly used this as practice for writing and SEO tactics. A few pages rank in the top ten on Google.",
+    excerpt: "This is a simple WordPress site. I mostly used this as practice for writing and SEO tactics. A few pages rank in the top ten on Google.",
     image: "/f_auto,q_auto/v1615707963/allaboutecho_nskxxp.jpg",
     url: "https://allaboutecho.com"
   },
   {
     title: "Can I Automate",
     tags: ["UI/UX Design", "SEO", "Development"],
-    excerpt: "This project is currenly in development. It's modeled after pcpartpicker.com. I'm hoping it can become the go to source for people looking to get into smart homes and automation. This is being built with React(NextJS) and MongoDB.",
+    excerpt: "This project is currently in development. I Modeled after pcpartpicker.com; I'm hoping it can become the go-to source for people getting into smart homes and automation. Can I Automate is built with React(NextJS) and MongoDB.",
     image: "/c_scale,f_auto,q_auto,w_1346/v1615707963/cia_zjpbvh.jpg",
     url: "https://caniautomate.com"
   },
   {
     title: "accruabl",
     tags: ["Design", "Logo", "Development"],
-    excerpt: "I did everything on this one. Logo, design, development. Built with React and Firebase’s Realtime Database.",
+    excerpt: "I did everything on this one, logo, design, and development. I Built it with React and Firebase’s Real-time Database.",
     image: "/c_scale,f_auto,q_auto,w_1390/v1615707968/accruabl_diryj6.jpg",
     url: "https://budgets-1ec3a.web.app/"
   },
   {
     title: "HeyJunior",
     tags: ["UI/UX Design", "Logo", "Development"],
-    excerpt: "This project is currently in development. Built with NextJS, Prisma(PostgreSQL), and Stripe for payment processing.",
+    excerpt: "This project is currently in development. HeyJunior is being developed with NextJS, Prisma(PostgreSQL), and Stripe for payment processing.",
     image: "/c_scale,f_auto,q_auto,w_1346/v1615707963/heyjunior_jl3dme.jpg",
     url: "https://heyjunior.co"
   },
